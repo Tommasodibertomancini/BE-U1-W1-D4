68ca6739f8435a5dc926f578e74cfdaa1e40ae44
Tommasodibertomancini <tommydbm@gmail.com>
2025-02-13 15:24:43 +0100

    Add files via upload

---
+++ BE-U1-W1-D4/BE-U1-W1-D4/Models/LoginOperations.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_U1_W1_D4.Models
+{
+    static class LoginOperations
+    {
+        public static string Username { get; private set; }
+        private static string Password;
+        public static DateTime? LoginTime { get; private set; }
+        public static List<DateTime> Accessi { get; } = new List<DateTime>();
+        public static bool Autenticato => Username != null;
+
+        public static bool Login(string username, string password, string confermaPassword)
+        {
+            if (string.IsNullOrWhiteSpace(username) || password != confermaPassword)
+            {
+                Console.WriteLine("Errore: Username non valido o le password non coincidono.");
+                return false;
+            }
+
+            Username = username;
+            Password = password;
+            LoginTime = DateTime.Now;
+            Accessi.Add(LoginTime.Value);
+            Console.WriteLine("Login effettuato con successo.");
+            return true;
+        }
+
+        public static void Logout()
+        {
+            if (!Autenticato)
+            {
+                Console.WriteLine("Errore: Nessun utente attualmente loggato.");
+                return;
+            }
+            Username = null;
+            Password = null;
+            LoginTime = null;
+            Console.WriteLine("Logout effettuato con successo.");
+        }
+
+        public static void VerificaOraDataLogin()
+        {
+            if (!Autenticato)
+            {
+                Console.WriteLine("Errore: Nessun utente attualmente loggato.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"L'utente {Username} si è loggato in data {LoginTime.Value.ToShortDateString()} alle ore {LoginTime.Value.ToShortTimeString()}.");
+            }
+        }
+
+        public static void ListaAccessi()
+        {
+            if (Accessi.Count == 0)
+            {
+                Console.WriteLine("Nessun accesso precedente in archivio.");
+                return;
+            }
+            Console.WriteLine("Lista degli accessi:");
+            foreach (var accesso in Accessi)
+            {
+                Console.WriteLine($"- {accesso.ToShortDateString()} alle ore {accesso.ToShortTimeString()}");
+            }
+        }
+    }
+}
---
+++ BE-U1-W1-D4/BE-U1-W1-D4/Program.cs
@@ -0,0 +1,48 @@
+﻿using BE_U1_W1_D4.Models;
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("\n===============OPERAZIONI==============");
+    Console.WriteLine("Scegli l'operazione da effettuare:");
+    Console.WriteLine("1.: Login");
+    Console.WriteLine("2.: Logout");
+    Console.WriteLine("3.: Verifica ora e data di login");
+    Console.WriteLine("4.: Lista degli accessi");
+    Console.WriteLine("5.: Esci");
+    Console.WriteLine("========================================");
+
+    Console.Write("Operazione: ");
+    string selezionato = Console.ReadLine();
+
+    switch (selezionato)
+    {
+        case "1":
+            Console.WriteLine("Inserisci UserName:");
+            string username = Console.ReadLine();
+            Console.WriteLine("Inserisci Password:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Conferma Password:");
+            string confermaPassword = Console.ReadLine();
+            LoginOperations.Login(username, password, confermaPassword);
+            break;
+        case "2":
+            LoginOperations.Logout();
+            break;
+        case "3":
+            LoginOperations.VerificaOraDataLogin();
+            break;
+        case "4":
+            LoginOperations.ListaAccessi();
+            break;
+        case "5":
+            Console.WriteLine("Arrivederci!");
+            return;
+        default:
+            Console.WriteLine("Operazione non valida.");
+            break;
+    }
+    Console.WriteLine("\nPremi un tasto per continuare...");
+    Console.ReadKey();
+}
